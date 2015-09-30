d2a0bd5536479b411e1c4580563596ffab423532
babassmine <bassco10@gmail.com>
2015-09-30 09:07:22 -0400

    make it better

--- gits.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "# toy_app_rails" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/babassmine/toy_app_rails.git
-git push -u origin master
